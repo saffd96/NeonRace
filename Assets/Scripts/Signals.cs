26dba6aafaac5603d1671a40b0acda4ff768aa15
Sasha-Home <saffd@tut.by>
2022-11-09 01:35:44 +0300

    first Commit

---
+++ Assets/Scripts/Signals.cs
@@ -0,0 +1,11 @@
+﻿public readonly struct InputSignal
+{
+    public readonly PlayerInput CurrentInput;
+    public readonly float Percentage;
+
+    public InputSignal(PlayerInput input, float percentage)
+    {
+        CurrentInput = input;
+        Percentage = percentage;
+    }
+}
